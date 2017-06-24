ddd45d7721250ec10c71642f12f50982bbade4bf
peterliuxb <woairuidaren@gmail.com>
2017-06-24 11:52:14 +0800

    seperate js file

---
+++ js/map.js
@@ -0,0 +1,28 @@
+
+  var map,marker;
+  function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.7413549, lng: -73.9980244},
+      zoom: 13
+    });
+    var tribeca = {lat: 40.719526, lng: -74.0089934};
+      marker = new google.maps.Marker({
+        position: tribeca,
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        title: 'First Marker!'
+      });
+      marker.addListener('click',toggleBounce);
+    }
+
+function toggleBounce(){
+  if(marker.getAnimation() != null){
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+window.addEventListener('load', initMap);
