573a4ecdabaf3707b968490ae3e440eb88ba7b1f
peterliuxb <woairuidaren@gmail.com>
2017-06-30 16:33:17 +0800

    change the app.js file

--- js/app.js
+++ js/app.js
@@ -1,16 +1,145 @@
-
+  //define global var
   var map;
-  var markersArray = [];
+  var infoWindow;
 
-  function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?js?libraries=places,geometry&key=AIzaSyDbC-ePyIjHbJQePaosaWn8XAb3GGWhRDI' +
-      '&&callback=initMap';
-  document.body.appendChild(script);
-}
-window.onload = loadScript;
+  var locations = [
+      {
+      title: "Evergreen Maritime Museum",
+      lat: 25.038611,
+      lng: 121.518889,
+      streetAddress: "Zhongzheng District, ",
+      cityAddress: "Taipei City, Taiwan 100"
+      },
+      {
+      title: "The Peace Park",
+      lat: 25.040363,
+      lng: 121.515469,
+      streetAddress: "No. 3, Ketagalan Blvd, Zhongzheng District,",
+      cityAddress: "Taipei City, Taiwan 100"
+      },
+      {
+      title: "The Taiwan Museum",
+      lat: 25.038234,
+      lng: 121.519291,
+      streetAddress: "No. 1, Section 1, Xinyi Road",
+      cityAddress: "Zhongzheng District, Taipei City, Taiwan 100"
+      },
+      {
+      title: "Melange Café",
+      lat: 25.049061,
+      lng: 121.517478,
+      streetAddress: "No. 23, Lane 16, Section 2, Zhongshan North Road,",
+      cityAddress: " Zhongshan District, Taipei City, Taiwan 10491 "
+      },
+      {
+      title: "starbucks",
+      lat: 25.045162,
+      lng: 121.505572,
+      streetAddress: "No. 77, Section 2, Wuchang St, ",
+      cityAddress: "Wanhua District, Taipei City, Taiwan 108"
+      },
+      {
+      title: "Taipei Cinema Park",
+      lat: 25.044722,
+      lng: 121.503137,
+      streetAddress: "No. 19, Kangding Road,",
+      cityAddress: " Wanhua District, Taipei City, Taiwan 108"
+      },
+      {
+      title: "hotel",
+      lat: 25.046775,
+      lng: 121.505803 ,
+      streetAddress: "No. 46, Kunming Street",
+      cityAddress: " Wanhua District, Taipei City, Taiwan 108"
+      },
+      {
+      title: "hotel",
+      lat: 25.042763,
+      lng: 121.509436,
+      streetAddress: "No. 4, Xiushan Street, ",
+      cityAddress: "Zhongzheng District, Taipei City, Taiwan 100"
+      },
+      {
+      title: "pizza ",
+      lat: 25.049061,
+      lng: 121.517478,
+      streetAddress: "103, Taiwan, Taipei City,,",
+      cityAddress: " Datong District, Section 1, Chengde Road"
+      }
+    ];
+
+  // function loadScript() {
+  // var script = document.createElement('script');
+  // script.type = 'text/javascript';
+  // script.src = 'https://maps.googleapis.com/maps/api/js?js?libraries=places,geometry&key=AIzaSyDbC-ePyIjHbJQePaosaWn8XAb3GGWhRDI' +
+  //     '&&callback=initMap' ;
+  // script.onerror = googleError();
+  // document.body.appendChild(script);
+  // }
+  // window.onload = loadScript;
   //Initialize the map and its contents
+
+  var Location = function(location){
+      var self = this;
+      self.title = ko.observable(location.title);
+      self.lat = ko.observable(location.lat);
+      self.lng = ko.observable(location.lng);
+      self.active = ko.observable(false);
+
+      // ajax callback
+      self.getContent = function(callback){
+        if(self.content){
+          return self.content;
+        }
+
+      var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + self.title() + '&format=json&callback=wikiCallback';
+
+      $.ajax({
+        url: wikiUrl,
+        dataType: 'jsonp',
+      })
+      .done(function(response){
+        var wikiContent = '';
+        if(response){
+          if(typeof response[1] !== 'undefined' && typeof response[3] !== 'undefined'){
+            for(var i = 0;i < 3;i++){
+              if(typeof response[1][i] !== 'undefined' && response[3][i] !== 'undefined'){
+                wikiContent += '<a href=">' + response[3][i] + '" target"_blank"' + response[1][i] + '</a><br>'
+              }
+            }
+          }
+        }
+        if(wikiContent != ''){
+          self.content = ko.observable('<h4>Wikipedia results for "' + self.title() + '"</h4><p>' + wikiContent + '</p>');
+        } else {
+          self.content = ko.observable('<h4>Wikipedia results for "' + self.title() + '"</h4><p>There was a problem reaching wikipedia</p>');
+        }
+      })
+      .fail(function() {
+        console.log("There is a error when call wiki api");
+        self.content = ko.observable('<h4>Wikipedia results for "' + self.title() + '"</h4><p>There was a problem reaching wikipedia</p>');
+      })
+      // return a spinner for while the external API is still loading
+      return '<h4>Wikipedia results for "' + self.title() + '"</h4><p><span class="spinner"></span></p>';
+      }
+
+      self.createMarker = (function(){
+        self.marker = new google.maps.Marker({
+          position: {lat:self.lat(), lng: self.lng()},
+          map:map,
+          title: self.title()
+        })
+
+        map.bounds.extend(self.marker.position);
+        // add click event listener to marker
+        self.marker.addListener('click',function(){
+          //selectLocation(self);
+        })
+
+      })();
+}
+
+  //define Google map
   function initMap() {
     var mapOptions = {
         zoom: 14,
@@ -18,276 +147,164 @@ window.onload = loadScript;
         mapTypeControl: false,
         disableDefaultUI: true
     };
-    // if($(window).width() <= 1080) {
-    //     mapOptions.zoom = 13;
-    // }
-    // if ($(window).width() < 850 || $(window).height() < 595) {
-    //     hideNav();
-    // }
-
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    setMarkers(markers);
+    map.bounds = new google.maps.LatLngBounds();
+    //infoWindow
+    infoWindow = new google.maps.InfoWindow({
+      content:''
+    })
 
-    setAllMap();
+    google.maps.event.addListener(infoWindow, 'closeclick', function(){
+        resetActiveState();
+    });
+
+    // add eventlistener to resize map when the browser resizes
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.fitBounds(map.bounds);
+    });
+
   }
 
-  //Determines if markers should be visible
-  //This function is passed in the knockout viewModel function
-  function setAllMap() {
-    for (var i = 0; i < markers.length; i++) {
-      if(markers[i].boolTest === true) {
-      markers[i].holdMarker.setMap(map);
-      } else {
-      markers[i].holdMarker.setMap(null);
-      }
-    }
-  }
+  //define VM to bind model and view
+  var ViewModel = function(){
+    self = this;
+    // show ui if map loaded properly
+    this.mapNotLoaded = ko.observable(false);
+    this.locationsList = ko.observableArray([]);
+    // add location objects to the locationsList
+    locations.forEach(function(item){
+      self.locationsList.push(new Location(item))
+    });
+    // fit map to new bounds
+    map.fitBounds(map.bounds);
 
+    this.currentLocation = ko.observable(locationsList()[0]);
 
-  //Information about the different locations
-  //Provides information for the markers
-  var markers = [
-      {
-      title: "Evergreen Maritime Museum",
-      lat: 25.038611,
-      lng: 121.518889,
-      streetAddress: "Zhongzheng District, ",
-      cityAddress: "Taipei City, Taiwan 100",
-      url: "www.nps.gov/thje/index.htm",
-      id: "nav0",
-      visible: ko.observable(true),
-      boolTest: true
-      },
-      {
-      title: "The Peace Park",
-      lat: 25.040363,
-      lng: 121.515469,
-      streetAddress: "No. 3, Ketagalan Blvd, Zhongzheng District,",
-      cityAddress: "Taipei City, Taiwan 100",
-      url: "www.nps.gov/linc/index.htm",
-      id: "nav1",
-      visible: ko.observable(true),
-      boolTest: true
-      },
-      {
-      title: "The Taiwan Museum",
-      lat: 25.038234,
-      lng: 121.519291,
-      streetAddress: "No. 1, Section 1, Xinyi Road",
-      cityAddress: "Zhongzheng District, Taipei City, Taiwan 100",
-      url: "www.nps.gov/wamo/index.htm",
-      id: "nav2",
-      visible: ko.observable(true),
-      boolTest: true
-      },
-      {
-      title: "Melange Café",
-      lat: 25.049061,
-      lng: 121.517478,
-      streetAddress: "No. 23, Lane 16, Section 2, Zhongshan North Road,",
-      cityAddress: " Zhongshan District, Taipei City, Taiwan 10491 ",
-      url: "www.visitthecapitol.gov",
-      id: "nav3",
-      visible: ko.observable(true),
-      boolTest: true
-      },
-      {
-      title: "starbucks",
-      lat: 25.045162,
-      lng: 121.505572,
-      streetAddress: "No. 77, Section 2, Wuchang St, ",
-      cityAddress: "Wanhua District, Taipei City, Taiwan 108",
-      url: "www.whitehouse.gov",
-      id: "nav4",
-      visible: ko.observable(true),
-      boolTest: true
-      },
-      {
-      title: "Taipei Cinema Park",
-      lat: 25.044722,
-      lng: 121.503137,
-      streetAddress: "No. 19, Kangding Road,",
-      cityAddress: " Wanhua District, Taipei City, Taiwan 108",
-      url: "www.wwiimemorial.com/",
-      id: "nav5",
-      visible: ko.observable(true),
-      boolTest: true
-      },
-      {
-      title: "hotel",
-      lat: 25.046775,
-      lng: 121.505803 ,
-      streetAddress: "No. 46, Kunming Street",
-      cityAddress: " Wanhua District, Taipei City, Taiwan 108",
-      url: "www.nga.gov/content/ngaweb.html",
-      id: "nav6",
-      visible: ko.observable(true),
-      boolTest: true
-      },
-      {
-      title: "hotel",
-      lat: 25.042763,
-      lng: 121.509436,
-      streetAddress: "No. 4, Xiushan Street, ",
-      cityAddress: "Zhongzheng District, Taipei City, Taiwan 100",
-      url: "www.mnh.si.edu",
-      id: "nav7",
-      visible: ko.observable(true),
-      boolTest: true
-      },
-      {
-      title: "pizza ",
-      lat: 25.049061,
-      lng: 121.517478,
-      streetAddress: "103, Taiwan, Taipei City,,",
-      cityAddress: " Datong District, Section 1, Chengde Road",
-      url: "www.si.edu/Museums/arts-and-industries-building",
-      id: "nav8",
-      visible: ko.observable(true),
-      boolTest: true
-      }
-  ];
+    // initialize searchTerm which is used to filter the list of locations displayed
+    this.searchTerm = ko.observable('');
 
-  var headingImageView = [5, 235, 55, 170, 190, 240, -10, 10, 190];
-  var streetViewImage;
-  var streetViewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=180x90&location=';
-
-  function determineImage() {
-      if (i === 3) {
-          streetViewImage = streetViewUrl + '38.892052,-77.008888&fov=75&heading=' + headingImageView[i] + '&pitch=10';
-        } else if (i === 4) {
-          streetViewImage = streetViewUrl +
-                        markers[i].streetAddress + ',' + markers[i].cityAddress +
-                        '&fov=75&heading=' + headingImageView[i] + '&pitch=10';
-                      } else {
-                        streetViewImage = streetViewUrl +
-                          markers[i].lat + ',' + markers[i].lng +
-                          '&fov=75&heading=' + headingImageView[i] + '&pitch=10';
-                        }
-        }
-
-
-
-    //Sets the markers on the map within the initialize function
-    //Sets the infoWindows to each individual marker
-    //The markers are inidividually set using a for loop
-    function setMarkers(location) {
-
-    for(i=0; i<location.length; i++) {
-        // create marker attr holdMarker
-        location[i].holdMarker = new google.maps.Marker({
-          position: new google.maps.LatLng(location[i].lat, location[i].lng),
-          map: map,
-          title: location[i].title,
-          icon: {
-            url: 'img/marker.png',
-            size: new google.maps.Size(25, 40),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(12.5, 40)
-            },
-          shape: {
-            coords: [1,25,-40,-25,1],
-            type: 'poly'
-          }
-        });
-
-        //test
-        //API Keys
-
-        //function to place google street view images within info windows
-        determineImage();
-
-        //Binds infoWindow content to each marker
-        location[i].contentString = '<img src="' + streetViewImage +
-                                  '" alt="Street View Image of ' + location[i].title + '"><br><hr style="margin-bottom: 5px"><strong>' +
-                                   location[i].title + '</strong><p>' +
-                                    location[i].streetAddress + '<br>' +
-                                    location[i].cityAddress + '<br></p><a class="web-links" href="http://' + location[i].url +
-                                    '" target="_blank">' + location[i].url + '</a>';
-
-        var infowindow = new google.maps.InfoWindow({
-            //content: markers[i].contentString
-            content:markers[i].contentString
-        });
-
-        //Click marker to view infoWindow
-        //zoom in and center location on click
-        new google.maps.event.addListener(location[i].holdMarker, 'click', (function(marker, i) {
-          return function() {
-            infowindow.setContent(location[i].contentString);
-            var address = {lat:location[i].lat, lng:location[i].lng};
-            // var panoramaOptions = {
-            //   position: address,
-            //   pov: {heading: 65, pitch: 0},
-            //   zoom:15
-            // };
-
-            // var panorama = new google.maps.StreetViewPanorama(
-            //   document.getElementById('pano'), panoramaOptions);
-
-            infowindow.open(map,this);
-            var windowWidth = $(window).width();
-            if(windowWidth <= 1080) {
-                map.setZoom(14);
-            } else if(windowWidth > 1080) {
-                map.setZoom(16);
-            }
-            map.setCenter(marker.getPosition());
-            location[i].picBoolTest = true;
-          };
-        })(location[i].holdMarker, i));
-
-        //Click nav element to view infoWindow
-        //zoom in and center location on click
-        var searchNav = $('#nav' + i);
-        searchNav.click((function(marker, i) {
-          return function() {
-            infowindow.setContent(location[i].contentString);
-            // var address = {lat:location[i].lat, lng:location[i].lng};
-            // var panoramaOptions = {
-            //   position: address,
-            //   pov: {heading: 65, pitch: 0},
-            //   zoom:15
-            // };
-
-            // var panorama = new google.maps.StreetViewPanorama(
-            //   document.getElementById('pano'), panoramaOptions);
-            infowindow.open(map,marker);
-            map.setZoom(16);
-            map.setCenter(marker.getPosition());
-            location[i].picBoolTest = true;
-          };
-        })(location[i].holdMarker, i));
-    }
-}
-
-    //Query through the different locations from nav bar with knockout.js
-    //only display markers and nav elements that match query result
-    var viewModel = {
-      query: ko.observable(''),
+    // this function is used to reset any active state that may be set
+    this.resetActiveState = function() {
+        self.currentLocation().active(false);
+        self.currentLocation().marker.setAnimation(null);
+        infowindow.close();
     };
 
-    viewModel.markers = ko.dependentObservable(function() {
-      var self = this;
-      //search 为输入框中的search value
-      var search = self.query().toLowerCase();
-      return ko.utils.arrayFilter(markers, function(marker) {
-        //如果有匹配的就返回那个marker
-        if (marker.title.toLowerCase().indexOf(search) >= 0) {
-            marker.boolTest = true;
-            return marker.visible(true);
+    this.filterLocation = ko.computed(function(){
+      //reset active marker
+      resetActiveState();
+
+      return self.locationsList().filter(function(location){
+        var display = true;
+        if(self.searchTerm() !== ''){
+          if(location().title().toLowerCase().index(self.searchTerm().toLowerCase()) !== -1){
+            display= true;
           } else {
-            marker.boolTest = false;
-            setAllMap();
-            return marker.visible(false);
+            display = false;
           }
-        });
-      }, viewModel);
+        }
+        location.marker.setVisible(display);
+        return display;
+      });
+    });
 
-      ko.applyBindings(viewModel);
+    // click handler for when a location is clicked
+    this.selectLocation = function(clickedlocation){
+      if(self.currentLocation() = clickedlocation && self.currentLocation().active == true){
+        resetActiveState();
+        return;
+      };
 
-//show $ hide markers in sync with nav
-  $("#input").keyup(function() {
-    setAllMap();
-  });
+      // reset any active state
+      resetActiveState();
+
+      // update currentLocation
+      self.currentLocation(clickedLocation);
+
+      self.currentLocation().active(true);
+
+      // bounce marker
+      self.currentLocation().marker.setAnimation(google.maps.Animation.BOUNCE);
+
+      // open infoWindow for the current location
+      infoWindow.setContent('<h1>' + self.currentLocation().title() + '</h1>' + self.currentLocation.getContent(function(l){
+        if(self.currentLocation() == l){
+          infoWindow.setContent('<h1>' + self.currentLocation().name() + '</h1>' + l.content())
+        }
+      }));
+
+      infoWindow.open(map, self.currentLocation().marker);
+
+      // center map on current marker
+      map.panTo(self.currentLocation().marker.position);
+
+    };
+    // hide nav initially on mobile
+    this.hideNav = ko.observable( window.innerWidth < 640 );
+    this.toggleNav = function() {
+      self.hideNav(! self.hideNav());
+      google.maps.event.trigger(map, 'resize');
+      map.fitBounds(map.bounds);
+    };
+};
+
+// This is called by the maps api as a callback
+var app = function() {
+  initMap();
+
+  ko.applyBindings(ViewModel);
+};
+
+
+// Fallback for Google Maps Api
+function googleError(){
+    console.log('Error: Google maps API has not loaded');
+    $('body').prepend('<p id="map-error">Sorry we are having trouble loading google maps API, please try again in a moment.</p>');
+};
+
+
+
+
+
+
+
+
+
+function getFouSquare(data) {
+  var CLIENT_ID = 'TT0MKMIFGVNZT0J2VJL3C4PIDKUX15G5VQGEHL3UFP4AQ5JA';
+  var CLIENT_SECRET = 'WD1PN1IS2JQDYYXITIWWPCZLKVBR1EJHMXOMYUTKFUFQC54N';
+
+  $.ajax({
+      type: "GET",
+      dataType: 'json',
+      cache: false,
+      url: 'https://api.foursquare.com/v2/venues/explore?',
+      data: 'limit=3&ll=' + data + '&radius=5000&venuePhotos=1&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=20160608&m=foursquare',
+      async: true
+    })
+      .done(function (response) {
+        var responseArr = response.response.groups[0].items;
+
+        for (var i = 0; i < responseArr.length; i++) {
+
+          var resArr = responseArr[i];
+          var fsphotosArr;
+
+          //self.fstitle(resArr.venue.name);
+          //self.fscontent(resArr.tips[0].text ? resArr.tips[0].text : "No tips here!");
+          fsphotosArr  = resArr.venue.photos.groups[0].items[0];
+          fsphotoURL = fsphotosArr.prefix + '100x100' + fsphotosArr.suffix;
+          //self.fsurl(resArr.tips[0].canonicalUrl);
+
+        }
+
+      })
+      .fail(function (response) {
+        //callback function if error - an alert will be activated to notify the user of the error
+        var foursquareDisplay =
+          '<p>Bummer!... Could not load data from Foursquare! Make sure you are connected to the Internet, or try again later.</p>';
+
+        //foursquareElem.append(foursquareDisplay);
+      });
+  }
